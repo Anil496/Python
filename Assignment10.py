be22e7aacd146863d7766449578ac9b7e721e428
Anilkumar <anilkumarroy85@gmail.com>
2022-08-31 05:05:50 +0530

    first commit

---
+++ Assignment10.py
@@ -0,0 +1,33 @@
+
+"""for MySirG in range(int(input("Enter a number"))):
+    print("MySirG")"""
+    
+"""for a in range(int(input("enter a natural number"))):
+    print(a+1,end=' ')"""
+
+"""for a in range(int(input( "enter a natural number")),0,-1):
+          print(a,end=' ')"""
+
+"""for a in range(int(input("Enter a number"))):
+    if a%2==1:
+        print(a)"""
+        
+"""for a in range(int(input("Enter a number")),0,-1):
+    if a%2==1:
+        print(a)"""
+
+"""for a in range(1,int(input("Enter a number")),1):
+    if a%2==0:
+        print(a)"""
+
+'''for a in range(int(input("Enter a number")),0,-1):
+    if a%2==0:
+        print(a)'''
+
+"""for a in range(int(input("Enter a number"))):
+        print((a+1)**2)"""
+
+'''for a in range(int(input("Enter a number"))):
+        print((a+1)**3)'''
+
+
